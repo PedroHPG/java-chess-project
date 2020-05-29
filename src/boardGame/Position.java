878eeeccc729a6ed8322d2fd28485587c71663a7
PedroHPG <pedrohpgoncalves@hotmail.com>
2020-05-29 10:15:17 -0300

    Added class Position

---
+++ src/boardGame/Position.java
@@ -0,0 +1,34 @@
+package boardGame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+
+}
